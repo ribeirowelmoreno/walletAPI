c666011e2afadf92b1d529a79d103e3b0813631b
Wellynton Ribeiro Moreno <ribeirowelmoreno@gmail.com>
2020-07-13 16:30:33 -0300

    Finalizado o repositorio o usario

--- src/test/java/com/wallet/WalletApplicationTests.java
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -1,5 +1,7 @@
 package com.wallet;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +15,7 @@ public class WalletApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
