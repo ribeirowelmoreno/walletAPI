7bbc52f9c04df889d121fb77ecab8de6b523b7fc
Wellynton Ribeiro Moreno <ribeirowelmoreno@gmail.com>
2020-07-14 11:46:13 -0300

    Criando o user service

--- src/test/java/com/wallet/WalletApplicationTests.java
+++ src/test/java/com/wallet/WalletApplicationTests.java
@@ -1,8 +1,6 @@
 package com.wallet;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -15,7 +13,6 @@ public class WalletApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		assertEquals(1, 1);
 	}
 
 }
